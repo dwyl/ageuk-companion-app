a5297839769888895cb037e952512411e2a6307b
andrewMacmurray <a.macmurray@icloud.com>
2016-06-28 19:12:59 +0100

    tests for basic server

---
+++ test/server/server.test.js
@@ -0,0 +1,35 @@
+const tape = require('tape');
+const server = require('../../lib/server.js');
+
+tape('server should start', (t) => {
+  t.plan(2);
+
+  server.inject({ url: '/', method: 'GET' }, (res) => {
+    t.ok(res, 'server responds');
+
+    const actual = res.statusCode;
+    const expected = 200;
+    t.equals(actual, expected, 'server responds with 200 status code');
+  });
+});
+
+tape('server should handle static assets', (t) => {
+  t.plan(2);
+
+  server.inject({ url: '/src/style.css', method: 'GET' }, (res) => {
+    const actual = res.statusCode;
+    const expected = 200;
+    t.equals(actual, expected, 'style.css recieved correctly');
+  });
+
+  server.inject({ url: '/src/js/main.js', method: 'GET' }, (res) => {
+    const actual = res.statusCode;
+    const expected = 200;
+    t.equals(actual, expected, 'main.js recieved correctly');
+  });
+});
+
+tape('teardown', (t) => {
+  server.stop();
+  t.end();
+});
