788c037ee758dea8e4110d7c612370161f1eb952
andrewMacmurray <a.macmurray@icloud.com>
2016-06-30 13:10:09 +0100

    #10 #18 hacking around with handlebars on the front end, trying to precompile tempates

---
+++ public/src/js/main.js
@@ -0,0 +1,11 @@
+// var source = document.getElementById('entry-template');
+// console.log(source);
+// var template = Handlebars.compile(source);
+// console.log(template);
+//
+var context = { title: 'my new post', body: 'ellie smells' };
+// var html = template(context);
+// console.log(html);
+//
+// var target = document.getElementById('handlebars-target');
+// target.innerHTML = html;
